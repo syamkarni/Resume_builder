770239c782bd3a18528a05aa4ee05f53763afb70
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-09-14 23:43:25 +0530

    changeees

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <Route path="/Project" element={<Project />} />
         <Route path="/EActivities" element={<EActivities />} />
         <Route path="/VData" element={<VData />} />
-        {/* comment here */}
+
       </Routes>
     </Router>
   );
